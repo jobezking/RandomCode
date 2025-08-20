790f0cfe9ecd410c7dccc20dac869b252c3b4b2a
jobezking <52982865+jobezking@users.noreply.github.com>
2025-08-20 00:11:36 -0400

    Add linear regression of dataset example

---
+++ imports.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+from scipy import stats
+from scipy.stats import chi2_contingency
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
