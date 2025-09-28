5897a5451a26b937f7c54bbdd01991df36ec54d3
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-28 17:52:26 -0400

    start

--- oo/supervised_worker.py
+++ oo/supervised_worker.py
@@ -1,3 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+class SupervisedWorker:   
+    def __init__(self):
+        pass
+
+
 def main():
     pass
 
