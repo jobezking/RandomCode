03b2bcc06763470f51cb051620bb1639497b356d
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-28 22:44:35 -0400

    update

--- oo/unsupervised_worker.py
+++ oo/unsupervised_worker.py
@@ -6,10 +6,31 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 class UnsupervisedWorker:   
-    def __init__(self):
-        pass
+    def __init__(self, csv_file, header=False):
+        self.csv_file = csv_file
+        self.header = header
+
+        # placeholders for data
+        self.X_d = None
+        self.X = None
+        self.Y = None
+        self.X_train = None
+        self.X_test = None
+        self.Y_train = None
+        self.Y_test = None
+
+    def load_data_unsupervised(self):
+       if self.header:
+          df = pd.read_csv(self.csv_file)
+       else:
+           df = pd.read_csv(self.csv_file, header=None)
+       X_d = df
+       
+       
 
 
+
+################
 def main():
     pass
 
