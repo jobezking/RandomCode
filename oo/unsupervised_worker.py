ca6881cd2c631c0a28660688aa76e49a128c6c98
Job King <jobezking@gmail.com>
2025-09-29 20:00:55 -0400

    start

--- oo/unsupervised_worker.py
+++ oo/unsupervised_worker.py
@@ -11,23 +11,43 @@ class UnsupervisedWorker:
         self.header = header
 
         # placeholders for data
-        self.X_d = None
-        self.X = None
-        self.Y = None
-        self.X_train = None
-        self.X_test = None
-        self.Y_train = None
-        self.Y_test = None
+        self.X_df = None
+        self.labels = None
+        self.centroids = None
 
     def load_data_unsupervised(self):
        if self.header:
-          df = pd.read_csv(self.csv_file)
+          self.X_df = pd.read_csv(self.csv_file)
        else:
-           df = pd.read_csv(self.csv_file, header=None)
-       X_d = df
-       
-       
+           self.X_df = pd.read_csv(self.csv_file, header=None)
 
+    def zscore_normalize(self):
+       return (self.X_df - self.X_df.mean()) / self.X_df.std()
+    
+    def runKMeans(self, K=3):
+        from sklearn.cluster import KMeans
+        kmeans = KMeans(K, random_state=42, n_init='auto')
+        kmeans.fit(self.X_df.values)
+        self.labels = kmeans.labels_
+        self.centroids = kmeans.cluster_centers_
+    
+    def plot_KMeans(self, K, output="Save"):
+        X = self.X_df.values
+        if X.shape[1] != 2:
+            raise ValueError("Data must be 2-dimensional for plotting.")
+        
+        plt.figure(figsize=(8, 6))
+        sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=self.labels, palette='viridis', s=100)
+        plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red', s=200, marker='X', label='Centroids')
+        plt.title('K-Means Clustering')
+        plt.legend()
+        if output == "Show":
+            plt.show()
+        elif output == "Both":
+            plt.show()
+            plt.savefig(f"K-means-{K}.png")
+        else:
+            plt.savefig(f"K-means-{K}.png")
 
 
 ################
