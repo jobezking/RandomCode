3ba922a277ee2d13c1b6500b60681c3b25b99cdd
Job King <jobezking@gmail.com>
2026-04-29 21:29:33 -0400

    update texttospeech.py

--- texttospeech/texttospeech.py
+++ texttospeech/texttospeech.py
@@ -1,11 +1,14 @@
 #requires pip install gTTS
 
 from gtts import gTTS
-import os
+#import os
 
-mytext = 'Welcome to GeeksforGeeks Joe!'
+with open('input.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+mytext = content
 language = 'en'
 
 myobj = gTTS(text=mytext, lang=language, slow=False)
-myobj.save("welcome.mp3")
-os.system("start welcome.mp3")
+myobj.save("input.mp3")
+#os.system("start input.mp3")
